4f066a75d884bc1d9948a50c6a2ca05d38814fb7
Sevdat <55320710+Sevdat@users.noreply.github.com>
2025-01-31 01:01:10 +0100

    improved structure

--- gnn.py
+++ gnn.py
@@ -4,120 +4,109 @@ import win32api
 import win32gui
 import win32con
 import time
-from scipy.spatial.transform import Rotation as R
+from typing import Optional, Tuple
+import quaternionic
 
 
 class Scene:
+    def __init__(self, point_cloud: Optional[o3d.geometry.PointCloud] = None, points: Optional[int] = None, colors: Optional[int] = None):
+        self.pcd: Optional[o3d.geometry.PointCloud] = None
+        self.window: Optional[o3d.visualization.VisualizerWithKeyCallback] = None
+        self.windowWidth: int = 800
+        self.windowHeight: int = 600
+        self.camera: Optional[Scene.Camera] = None
 
-    def __init__(self, pointCloud = None, points = None, colors = None):
-        global pcd
-        global camera
-        if pointCloud is not None :
-            pcd = o3d.geometry.PointCloud(pointCloud)
+        if point_cloud is not None:
+            self.pcd = point_cloud
         elif points is not None and colors is not None:
-            self.create_point_cloud(points,colors)
-        self.create_window()
-        camera = self.Camera()
-    
-    def create_point_cloud(self,points,colors):
-        global pcd
-        pointCloud = o3d.geometry.PointCloud()
-        pointCloud.points = o3d.utility.Vector3dVector(np.random.rand(points, 3))
-        pointCloud.colors = o3d.utility.Vector3dVector(np.random.rand(colors, 3))
-        pcd = o3d.geometry.PointCloud(pointCloud)
+            self.createPointCloud(points, colors)
 
-    def create_window(self):
-        global window
-        global window_width
-        global window_height
-        window_width = 800
-        window_height = 600
-        window = o3d.visualization.VisualizerWithKeyCallback()
-        window.create_window(width=window_width, height=window_height)
-        screen_width = win32api.GetSystemMetrics(0)
-        screen_height = win32api.GetSystemMetrics(1)
-        center_x = (screen_width - window_width) // 2
-        center_y = (screen_height - window_height) // 2
+        self.createWindow()
+        self.camera = Scene.Camera(self)
+
+    def createPointCloud(self, points: int, colors: int) -> None:
+        self.pcd = o3d.geometry.PointCloud()
+        self.pcd.points = o3d.utility.Vector3dVector(np.random.rand(points, 3))
+        self.pcd.colors = o3d.utility.Vector3dVector(np.random.rand(colors, 3))
+
+    def createWindow(self) -> None:
+        self.window = o3d.visualization.VisualizerWithKeyCallback()
+        self.window.create_window(width=self.windowWidth, height=self.windowHeight)
+
+        screenWidth = win32api.GetSystemMetrics(0)
+        screenHeight = win32api.GetSystemMetrics(1)
+        centerX = (screenWidth - self.windowWidth) // 2
+        centerY = (screenHeight - self.windowHeight) // 2
+
         hwnd = win32gui.FindWindow(None, "Open3D")
         if hwnd:
-            win32gui.SetWindowPos(hwnd, win32con.HWND_TOP, center_x, center_y, window_width, window_height, win32con.SWP_SHOWWINDOW)
-        window.add_geometry(pcd)
-        window_height -= 48
-        window_width -= 19
+            win32gui.SetWindowPos(hwnd, win32con.HWND_TOP, centerX, centerY, self.windowWidth, self.windowHeight, win32con.SWP_SHOWWINDOW)
+
+        if self.pcd:
+            self.window.add_geometry(self.pcd)
+
+        self.windowHeight -= 48
+        self.windowWidth -= 19
 
     class Camera:
+        def __init__(self, scene: 'Scene'):
+            self.scene = scene
+            self.fps: float = 1 / 60
+            self.sensitivity: float = self.fps * 2
+            self.forward: float = 0
+            self.right: float = 0
+            self.up: float = 0
+            self.mouseLookX: float = 0
+            self.mouseLookY: float = 0
+            self.mouseScrollX: float = 0
+            self.mouseScrollY: float = 0
+            self.mouseYMoreThan180: bool = False
+            self.ctr = self.scene.window.get_view_control()  # IDE should recognize this now
 
-        def __init__(self):
-            global fps 
-            global sensitivity 
-            global forward 
-            global right 
-            global up 
-            global mouseLookX 
-            global mouseLookY 
-            global mouseScrollY
-            global mouseScrollX
-            global mouseYMoreThan180 
-            global ctr
-            fps = 1/60
-            sensitivity = fps*2
-            forward = 0
-            right = 0
-            up = 0
-            mouseLookX = 0
-            mouseLookY = 0
-            mouseScrollX = 0
-            mouseScrollY = 0
-            mouseYMoreThan180 = 0
-            self.camera_controls()
-            self.keyboard_controls = self.Camera_Keyboard_Controls()
-            self.touchpad_controls = self.Camera_TouchPad_Controls()
-            self.set_Render_Distance(0.1,1000)
+            self.keyboard_controls = Scene.Camera.CameraKeyboardControls(self)
+            self.touchpad_controls = Scene.Camera.CameraTouchPadControls(self)
+            self.setRenderDistance(0.1, 1000)
 
-        def camera_controls(self):
-            global ctr
-            ctr = window.get_view_control()
+        def cameraFront(self, x: float, y: float, z: float) -> None:
+            self.ctr.set_front([x, y, z])
 
-        def camera_front(self,x,y,z):
-            ctr.set_front([x, y, z])
+        def cameraLookat(self, x: float, y: float, z: float) -> None:
+            self.ctr.set_lookat([x, y, z])
 
-        def camera_lookat(self,x,y,z):
-            ctr.set_lookat([x,y,z])
+        def cameraSetUp(self, x: float, y: float, z: float) -> None:
+            self.ctr.set_up([x, y, z])
 
-        def camera_set_Up(self,x,y,z):
-            ctr.set_up([x,y,z])
+        def cameraSetZoom(self, zoom: float) -> None:
+            self.ctr.set_zoom(zoom)
 
-        def camera_set_Up(self,zoom):
-            ctr.set_zoom(zoom)
-        
-        def set_Render_Distance(self,near,far):
-            ctr.set_constant_z_near(near)
-            ctr.set_constant_z_far(far)
-        
-        def get_camera_origin_and_orientation(self):
-            extrinsic_matrix = ctr.convert_to_pinhole_camera_parameters().extrinsic
+        def setRenderDistance(self, near: float, far: float) -> None:
+            self.ctr.set_constant_z_near(near)
+            self.ctr.set_constant_z_far(far)
 
-            rotation_matrix = extrinsic_matrix[:3, :3]
-            origin = extrinsic_matrix[:3, 3]
+        def getOriginAndDirection(self) -> dict:
+            extrinsicMatrix = self.ctr.convert_to_pinhole_camera_parameters().extrinsic
+            rotationMatrix = extrinsicMatrix[:3, :3]
+            origin = extrinsicMatrix[:3, 3]
 
-            right_direction = rotation_matrix[:, 0]
-            right_direction = right_direction / np.linalg.norm(right_direction)
+            rightDirection = rotationMatrix[:, 0]
+            rightDirection = rightDirection / np.linalg.norm(rightDirection)
 
-            up_direction = rotation_matrix[:, 1]
-            up_direction = up_direction / np.linalg.norm(up_direction)
+            upDirection = rotationMatrix[:, 1]
+            upDirection = upDirection / np.linalg.norm(upDirection)
 
-            forward_direction = -rotation_matrix[:, 2]
-            forward_direction = forward_direction / np.linalg.norm(forward_direction)
+            forwardDirection = -rotationMatrix[:, 2]
+            forwardDirection = forwardDirection / np.linalg.norm(forwardDirection)
 
             return {
                 "origin": origin,
-                "right": right_direction,
-                "up": up_direction,
-                "forward": forward_direction
+                "right": rightDirection,
+                "up": upDirection,
+                "forward": forwardDirection
             }
-        class Camera_Keyboard_Controls:
-            
-            def __init__(self):
+
+        class CameraKeyboardControls:
+            def __init__(self, camera: 'Scene.Camera'):
+                self.camera = camera
                 key_actions = {
                     ord('W'): lambda vis: self.key_callback(vis, ord('W')),
                     ord('S'): lambda vis: self.key_callback(vis, ord('S')),
@@ -127,61 +116,57 @@ class Scene:
                     ord('Q'): lambda vis: self.key_callback(vis, ord('Q')),
                 }
                 for key, action in key_actions.items():
-                    window.register_key_callback(key, action)
-                
-            def key_callback(self,vis, key_code):
-                global forward
-                global right
-                global up
+                    self.camera.scene.window.register_key_callback(key, action)
+
+            def key_callback(self, vis, key_code: int) -> bool:
                 if key_code == ord('W'):
-                    forward = 3
+                    self.camera.forward = 3
                 elif key_code == ord('S'):
-                    forward = -3
+                    self.camera.forward = -3
                 elif key_code == ord('D'):
-                    right = 3
+                    self.camera.right = 3
                 elif key_code == ord('A'):
-                    right = -3
+                    self.camera.right = -3
                 elif key_code == ord('E'):
-                    up = 3
+                    self.camera.up = 3
                 elif key_code == ord('Q'):
-                    up = -3
-                ctr.camera_local_translate(forward,right,up)
-                right = 0
-                forward = 0
-                up = 0
+                    self.camera.up = -3
+
+                self.camera.ctr.camera_local_translate(self.camera.forward, self.camera.right, self.camera.up)
+                self.camera.right = 0
+                self.camera.forward = 0
+                self.camera.up = 0
                 return False
-            
-        class Camera_TouchPad_Controls:
 
-            def __init__(self):
-                window.register_mouse_move_callback(self.on_mouse_move)
-                window.register_mouse_scroll_callback(self.on_mouse_scroll)
-            
-            def on_mouse_move(self,obj, x, y):
-                global mouseLookY
-                global mouseLookX
+        class CameraTouchPadControls:
+            def __init__(self, camera: 'Scene.Camera'):
+                self.camera = camera
+                self.camera.scene.window.register_mouse_move_callback(self.on_mouse_move)
+                self.camera.scene.window.register_mouse_scroll_callback(self.on_mouse_scroll)
 
-                if (x>window_width/2): mouseLookX = (x-(window_width/2))*sensitivity
-                else :  mouseLookX = -((window_width/2)-x)*sensitivity
+            def on_mouse_move(self, obj, x: int, y: int) -> None:
+                if x > self.camera.scene.windowWidth / 2:
+                    self.camera.mouseLookX = (x-(self.camera.scene.windowWidth/2))*self.camera.sensitivity
+                else:
+                    self.camera.mouseLookX = -((self.camera.scene.windowWidth/2)-x)*self.camera.sensitivity
 
-                if (y>window_height/2): mouseLookY = (y-(window_height/2))*sensitivity
-                else : mouseLookY = -((window_height/2)-y)*sensitivity
+                if y > self.camera.scene.windowHeight / 2:
+                    self.camera.mouseLookY = (y-(self.camera.scene.windowHeight/2))*self.camera.sensitivity
+                else:
+                    self.camera.mouseLookY = -((self.camera.scene.windowHeight/2)-y)*self.camera.sensitivity
 
-            def on_mouse_scroll(self,obj, x, y):
-                global mouseScrollY
-                global mouseScrollX
-                mouseScrollX += x*sensitivity/10
-                mouseScrollY += y*sensitivity/10
-                print(y)
-                
+            def on_mouse_scroll(self, obj, x: int, y: int) -> None:
+                self.camera.mouseScrollX -= x*self.camera.sensitivity/10
+                self.camera.mouseScrollY -= y*self.camera.sensitivity/10
+                print(self.camera.getOriginAndDirection())
 
 
-Scene(o3d.geometry.PointCloud(o3d.io.read_point_cloud(o3d.data.PLYPointCloud().path)))
-
+# Example usage
+scene = Scene(o3d.geometry.PointCloud(o3d.io.read_point_cloud(o3d.data.PLYPointCloud().path)))
 
 while True:
-    window.poll_events()
-    window.update_renderer()
-    ctr.camera_local_rotate(mouseLookX,mouseLookY)
-    ctr.camera_local_translate(mouseScrollY,mouseScrollX,0)
-    time.sleep(fps)
+    scene.window.poll_events()
+    scene.window.update_renderer()
+    scene.camera.ctr.camera_local_rotate(scene.camera.mouseLookX, scene.camera.mouseLookY)
+    scene.camera.ctr.camera_local_translate(scene.camera.mouseScrollY, scene.camera.mouseScrollX, 0)
+    time.sleep(scene.camera.fps)
